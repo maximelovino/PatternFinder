cc9148a342e9d12ce2fa34c1941788471c614271
Lovino Maxime <maximelovino@gmail.com>
2017-05-08 00:10:55 +0200

    Finishing boyerMoore tomorrow morning

--- src/ch/hepia/it/PatternFinding/Engine/BoyerMoore.java
+++ src/ch/hepia/it/PatternFinding/Engine/BoyerMoore.java
@@ -24,6 +24,16 @@ public class BoyerMoore extends PatternFinder {
 		for (int i = pattern.length() - 1; i >= 0; i--) {
 			Character currentChar = pattern.charAt(i);
 			map1.putIfAbsent(currentChar, pattern.length() - 1 - i);
+			int moveLeft = findSubstringMoveFromRight(pattern.substring(i), i);
+			System.out.println(pattern.substring(i) + "\t" + moveLeft);
 		}
+	}
+
+	private int findSubstringMoveFromRight (String substring, int startIndex) {
+		for (int i = startIndex - substring.length(); i >= 0; i--) {
+			if ((i != 0 && pattern.charAt(i - 1) != pattern.charAt(startIndex - 1)) && (pattern.substring(i, i + substring.length()).equals(substring)))
+				return startIndex - i;
+		}
+		return startIndex + 1;
 	}
 }
